307d28c1a43646473656511016a78cd2dd56a179
Sazzad Hossain Abid <83271585+Abid05@users.noreply.github.com>
2021-07-02 12:42:45 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:25,
+  nav:false,
+  autoplay:true,
+  autoplayTimeout: 3000,
+  dotsEach:true,
+  pullDrag:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:2
+      }
+  }
+})
